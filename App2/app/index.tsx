a10eabf720ec87179bad39b3bab45bccbcb9f89f
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2026-07-08 04:00:06 +0530

    App2

---
+++ App2/app/index.tsx
@@ -0,0 +1,20 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+
+
+  let name = "Chinthaka"
+  function m(){
+
+    let a = 10;
+    console.log(a)
+
+  }
+
+ 
+  console.log(name)
+
+  m()
+
+}
