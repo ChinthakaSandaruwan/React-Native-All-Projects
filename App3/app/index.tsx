7fcd0ad97e2cb680965671c58eed3af2fffd23a1
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-08 13:09:52 +0530

    App3

---
+++ App3/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ backgroundColor: "red", height: 100 }}>
+      <Text style={{ fontSize: 20, marginTop: 70, marginLeft: 50 }}>
+        Hello, React Native!
+      </Text>
+       <Text style={{ fontSize: 20, marginTop: 70, marginLeft: 50 }}>
+        Hello, React Native!
+      </Text>
+    </View>
+
+    
+
+    
+  );
+}
