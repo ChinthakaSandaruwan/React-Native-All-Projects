8509af8ceece597958b783e9df916f4010d8d859
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-08 10:54:17 +0530

    App2

--- App2/app/index.tsx
+++ App2/app/index.tsx
@@ -2,19 +2,35 @@ import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  let name: string = "Chinthaka";
+  let age: number = 24;
+  let height: number = 60.25;
+  let isStudent: boolean = true;
 
+  let email: string = "chinthakasw000@gmail.com";
+  // without type
+  let gmail = "chinthakasw000@gmail.com";
 
-  let name = "Chinthaka"
-  function m(){
+  let scores: number[] = [10, 20, 30, 44, 55, 80];
+  // without type
+  let scores2 = [10, 20, 30, 44, 55, 80];
 
-    let a = 10;
-    console.log(a)
+  let subjects: string[] = ["oop", "java", "pm", "maths"];
+  let subjects2 = ["Maths", "Science", "English", "Sinhala"];
 
+  function m() {}
+
+  function n(): void {}
+
+  function X(): string {
+    return "Hello";
   }
 
- 
-  console.log(name)
+  function Y(mark: number) {}
 
-  m()
+  function z(mark: number, subject: string) {}
 
+  function z2(mark: number, subject: string): string {
+    return " 60 - OOP"; 
+  }
 }
