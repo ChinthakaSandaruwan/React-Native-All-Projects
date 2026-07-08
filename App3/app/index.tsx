28b176f1db26cf862fa1a68d7e0543beebffb52c
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-08 14:46:21 +0530

    App3

--- App3/app/index.tsx
+++ App3/app/index.tsx
@@ -1,18 +1,37 @@
-import { Text, View } from "react-native";
+import { Button, StatusBar, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
+
+    <SafeAreaView>
     <View style={{ backgroundColor: "red", height: 100 }}>
+      <StatusBar barStyle="light-content" backgroundColor="green" />
       <Text style={{ fontSize: 20, marginTop: 70, marginLeft: 50 }}>
         Hello, React Native!
       </Text>
-       <Text style={{ fontSize: 20, marginTop: 70, marginLeft: 50 }}>
-        Hello, React Native!
-      </Text>
+
+      <View style={{ backgroundColor: "blue", height: 100 }}>
+        <Text style={{ fontSize: 20, marginTop: 70, marginLeft: 50 }}>
+          Hello, React Native!
+        </Text>
+      </View>
+      <View>
+        <TextInput
+          style={{ backgroundColor: "gray" }}
+          placeholder="Name"
+        ></TextInput>
+      </View>
+
+      <View>
+        <Button
+          title="Login"
+          onPress={() => {
+            console.log("Pressed");
+          }}
+        ></Button>
+      </View>
     </View>
-
-    
-
-    
+    </SafeAreaView>
   );
 }
