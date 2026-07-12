96e2ac33965511833aca8e6717a9ec226b0a370d
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-12 13:18:58 +0530

    App 9

--- App8/app/index.tsx
+++ App8/app/index.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
+
+  
+  
+    const [value,setValue] = useState(0);
+
+    useEffect(()=>{
+
+            let value = 1;
+
+         const timer =   setInterval(()=>{
+                console.log(value++)
+            },1000);
+
+                return(()=>{
+                    clearInterval(timer);
+                })
+
+    },[]);
+
+    return (
+
     <SafeAreaView style={styles.container}>
-      <Pressable style={styles.btn}>
-        <Text style={styles.btnText}></Text>
+
+        <Text style={styles.text}>{value}</Text>
+
+      <Pressable style={styles.btn}
+      
+        onPress={()=>{
+            setValue( value +1)
+            
+        }}
+      >
+        <Text style={styles.btnText}>Button</Text>
       </Pressable>
+
+
     </SafeAreaView>
   );
 }
@@ -15,16 +47,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+     justifyContent: "center",
   },
 
   btn: {
     backgroundColor: "blue",
     padding: 10,
     alignItems: "center",
+    borderRadius:20,
+    marginTop:20,
   },
 
   btnText: {
     color: "white",
     fontWeight: "bold",
   },
+
+  text:{
+    alignItems:"center",
+    justifyContent:"center",
+    fontWeight:"bold",
+    textAlign:"center"
+  }
 });
