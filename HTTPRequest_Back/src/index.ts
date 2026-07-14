8cd780cd00f67c23bdc82c07d7ae5917564f6fcc
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-14 13:37:51 +0530

    Resolve merge conflict in src/index.ts

--- HTTPRequest_Back/src/index.ts
+++ HTTPRequest_Back/src/index.ts
@@ -5,26 +5,14 @@ const app = express();
 
 app.use(express.json());
 
-<<<<<<< HEAD
-app.get("/",(req,res)=>{
-
-    res.send("Welcome to HTTPRequest BackEnd")
-});
-
-app.listen(3000,()=>{
-
-    console.log("server running on http://localhost:3000 ");
-
-});
-=======
 app.get("/", (req, res) => {
   res.send("Welcome to HTTP Request BackEnd");
 });
 
-//  http:// 192.168.1.13:3000/products
+//  http://192.168.1.13:3000/products
 app.use("/products", productRoute);
 
 app.listen(3000, () => {
-  console.log("server running on http://192.168.1.13:3000 ");
+  console.log("server running on http://192.168.1.13:3000");
 });
->>>>>>> 53b7eefa0a3fa6b5f6f56d3b79557e66f4aec13a
+
