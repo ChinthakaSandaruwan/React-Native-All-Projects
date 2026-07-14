6e6de2d3875fb634fd69a492b63b4dae4a4549e4
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-14 13:34:13 +0530

    hvhhf

--- HTTPRequest_Back/src/index.ts
+++ HTTPRequest_Back/src/index.ts
@@ -4,8 +4,13 @@ const app = express();
 
 app.use(express.json);
 
+app.get("/",(req,res)=>{
+
+    res.send("Welcome to HTTPRequest BackEnd")
+});
+
 app.listen(3000,()=>{
 
     console.log("server running on http://localhost:3000 ");
-    
+
 });
