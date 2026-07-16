681f8bb95ccbe2af110396ff8d0f10cc29a3739a
chinthakasw000 <chinthakasw000@gmail.com>
2026-07-16 18:24:54 +0530

    meka nam pissuvak bm

---
+++ FetchPostBack/src/index.ts
@@ -0,0 +1,10 @@
+import  express  from "express";
+
+const app = express();
+
+app.listen(5000,()=>{
+
+    console.log("back end started");
+    console.log("Server URL: http://localhost:5000");
+
+});
